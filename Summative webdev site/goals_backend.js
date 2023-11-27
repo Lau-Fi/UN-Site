7d63943deeb245000f13374821143055293397c7
Laurence Beven <laurenc33e@outlook.com>
2023-11-27 00:04:31 +0000

    Buttons, page section and media query! - Laurence

--- Summative webdev site/goals_backend.js
+++ Summative webdev site/goals_backend.js
@@ -1,5 +1,7 @@
 //JS backend for goals page
 
+//Page Heading
+
 let testp = document.createElement("h2");
 
 testp.textContent = "Goals";
@@ -8,7 +10,30 @@ let bodyElement = document.querySelector("h2");
 
 bodyElement.parentNode.insertBefore(testp, bodyElement);
 
-
+/// Page logo
 var image = document.createElement('img');
 image.src = "Website_Logo.png"
-document.getElementById('hi').appendChild(image)
+image.id = "logo"
+document.getElementById('logo').appendChild(image)
+
+
+/// Navigation buttons
+
+var h_button = document.createElement("button")
+h_button.textContent = "Home"
+document.getElementById('home_button').appendChild(h_button)
+
+var a_button = document.createElement("button")
+a_button.textContent = "About Us"
+document.getElementById('about_button').appendChild(a_button)
+
+var g_button = document.createElement("button")
+g_button.textContent = "Goals"
+document.getElementById('goals_button').appendChild(g_button)
+
+var s_button = document.createElement("button")
+s_button.textContent = "Sign Up"
+document.getElementById('signup_button').appendChild(s_button)
+
+/// Background split
+
